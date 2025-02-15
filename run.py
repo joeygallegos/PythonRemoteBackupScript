af1bbd5731f6f8718c3aed28d4d3e9e7ebd5cb42
Joey Gallegos <joey@joeygallegos.com>
2025-02-14 23:19:05 -0600

    Improvements to script

--- run.py
+++ run.py
@@ -15,6 +15,13 @@ def get_config():
         return json.load(file)
 
 
+def show_warning(message):
+    try:
+        ctypes.windll.user32.MessageBoxW(0, str(message), "Warning", 0x40 | 0x1)
+    except Exception as e:
+        print(f"Failed to show warning: {e}")
+
+
 def backup_data(config):
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None  # Be cautious with this line in a production environment
@@ -52,84 +59,82 @@ def backup_data(config):
             if server.get("commands_on_connect"):
                 logging.info(f"Found startup commands, running")
                 for connect_command in server.get("commands_on_connect"):
-                    command_output = (
-                        "Command: "
-                        + str(connect_command)
-                        + ": "
-                        + str(sftp.execute(connect_command))
-                    )
-                    logging.info(command_output)
+                    command_output = sftp.execute(connect_command)
+                    logging.info(f"Command: {connect_command}: {command_output}")
 
             remote_dir = server.get("backup_dir")
-            if sftp.exists(remote_dir):
-                files_to_backup = server.get("files", [])
-                # Loop through file patterns and backup the latest file found that matches that pattern
-                for file_pattern in files_to_backup:
-                    latest_file_bytes = sftp.execute(
-                        f"ls -t {remote_dir}/{file_pattern} | head -n 1"
-                    )[0]
-                    latest_file = latest_file_bytes.decode().strip()
-                    if latest_file:
-                        logging.info(
-                            f"Latest file matching {file_pattern} on {hostname}: {latest_file}"
-                        )
-
-                        # Generate a 10-character nonce
-                        alphabet = string.ascii_letters + string.digits  # A-Z, a-z, 0-9
-                        nonce = "".join(secrets.choice(alphabet) for i in range(10))
-
-                        local_file_name = f"{hostname}_{latest_file.split('/')[-1].replace('.gz', f'_{nonce}.gz')}"
-                        local_file_path = os.path.join(
-                            config["local_backup_dir"], local_file_name
-                        )
-
-                        sftp.get(latest_file, localpath=local_file_path)
-                        logging.info(f"File downloaded to {local_file_path}")
-
-                        latest_file_size = os.path.getsize(local_file_path)
-
-                        # Determine the 25% size reduction threshold
-                        size_threshold = (
-                            latest_file_size * 0.75
-                        )  # 25% smaller than the latest file size
-
-                        # Check the sizes of the last 5 files
-                        previous_files_bytes = sftp.execute(
-                            f"ls -t {remote_dir}/{file_pattern} | head -n 6 | tail -n 5"
-                        )[0]
-                        previous_files = (
-                            previous_files_bytes.decode().strip().split("\n")
-                        )
-
-                        # Sanity check that the file is a valid size compared to the last backup we took
-                        smaller_file_found = False
-                        for file in previous_files:
-                            remote_file_path = os.path.join(remote_dir, file)
-                            remote_file_size = sftp.stat(remote_file_path).st_size
-                            if remote_file_size < size_threshold:
-                                smaller_file_found = True
-                                break
-
-                        if smaller_file_found:
-                            error_message = f"Some recent files matching {file_pattern} on {hostname} are at least 25% smaller in size than the latest file. Please verify the downloaded file."
-                            print(
-                                f"{Fore.RED}{Style.BRIGHT}WARNING: {error_message}{Style.RESET_ALL}"
-                            )
-                            ctypes.windll.user32.MessageBoxW(
-                                0, error_message, "Warning", 0x40 | 0x1
-                            )
-                    else:
-                        logging.warning(
-                            f"No files matching {file_pattern} found on {hostname}"
-                        )
-            else:
+            if not sftp.exists(remote_dir):
                 error_message = (
                     f"Remote directory {remote_dir} does not exist on {hostname}"
                 )
                 logging.error(f"{Fore.RED}{error_message}{Style.RESET_ALL}")
-                ctypes.windll.user32.MessageBoxW(
-                    0, error_message, "Warning", 0x40 | 0x1
-                )
+                show_warning(error_message)
+                continue  # Skip to the next server
+
+            files_to_backup = server.get("files", [])
+            for file_pattern in files_to_backup:
+                try:
+                    latest_file_output = sftp.execute(
+                        f"ls -t {remote_dir}/{file_pattern} | head -n 1"
+                    )
+                    if not latest_file_output:
+                        show_warning(
+                            f"No files matching {file_pattern} found on {hostname}"
+                        )
+                        raise FileNotFoundError(
+                            f"No files matching {file_pattern} found on {hostname}"
+                        )
+
+                    latest_file = latest_file_output[0].decode().strip()
+                    logging.info(
+                        f"Latest file matching {file_pattern} on {hostname}: {latest_file}"
+                    )
+
+                    # Generate a 10-character nonce
+                    alphabet = string.ascii_letters + string.digits
+                    nonce = "".join(secrets.choice(alphabet) for _ in range(10))
+
+                    local_file_name = f"{hostname}_{latest_file.split('/')[-1].replace('.gz', f'_{nonce}.gz')}"
+                    local_file_path = os.path.join(
+                        config["local_backup_dir"], local_file_name
+                    )
+
+                    sftp.get(latest_file, localpath=local_file_path)
+                    logging.info(f"File downloaded to {local_file_path}")
+
+                    latest_file_size = os.path.getsize(local_file_path)
+                    size_threshold = latest_file_size * 0.75
+
+                    # Check the sizes of the last 5 files
+                    previous_files_output = sftp.execute(
+                        f"ls -t {remote_dir}/{file_pattern} | head -n 6 | tail -n 5"
+                    )
+                    previous_files = [
+                        f.decode().strip() for f in previous_files_output if f
+                    ]
+
+                    smaller_file_found = any(
+                        sftp.stat(os.path.join(remote_dir, file)).st_size
+                        < size_threshold
+                        for file in previous_files
+                    )
+
+                    if smaller_file_found:
+                        error_message = f"Some recent files matching {file_pattern} on {hostname} are at least 25% smaller in size than the latest file. Please verify the downloaded file."
+                        print(
+                            f"{Fore.RED}{Style.BRIGHT}WARNING: {error_message}{Style.RESET_ALL}"
+                        )
+                        show_warning(error_message)
+
+                except FileNotFoundError as e:
+                    logging.warning(e)
+                except Exception as e:
+                    logging.error(
+                        f"Unexpected error while processing {file_pattern} on {hostname}: {e}"
+                    )
+                    show_warning(
+                        f"Unexpected error while processing {file_pattern} on {hostname}: {e}"
+                    )
 
 
 if __name__ == "__main__":
